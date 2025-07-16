dfa652c3008bb3d7498ed28ed317e65ac6c21c25
Nguyen Phu Quang <115868752+nguyenphuquang1234567@users.noreply.github.com>
2025-07-16 22:20:31 +0700

    Reduce input sending rate from 40Hz to 30Hz

--- game.js
+++ game.js
@@ -644,7 +644,7 @@ function sendInput() {
         socket.emit('playerInput', { color: myColor, input: { ...localInput } });
     }
 }
-setInterval(sendInput, 1000/40); // 40 times per second
+setInterval(sendInput, 1000/30); // 30 times per second
 
 // Receive remote input from server (by color)
 if (socket) {
